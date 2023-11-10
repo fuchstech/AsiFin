c3a6452f92228e81d525ce74648e4f9f4c4aebc6
Kerem Yıldız <49817710+fuchstech@users.noreply.github.com>
2023-11-10 03:33:20 +0300

    a

--- crypto/conf.py
+++ crypto/conf.py
@@ -1,13 +1,8 @@
-# yatırım miktarı 
-# tek seferde açık olması istenen tradeler
-# işlem yapılacak pair
-#
-# 
-
 import json
+
 def read_config(path):
     global data
-# JSON dosyasının içeriğini bir değişkene yükle
+    # JSON dosyasının içeriğini bir değişkene yükle
     with open(path) as f:
         data = json.load(f)
 
@@ -22,15 +17,32 @@ def read_config(path):
     print(f"dry_run_wallet: {dry_run_wallet}")
     print(f"pair_whitelist: {pair_whitelist}")
 
-def write_config(path,max_trades,stake_currency,dry_run_wallet,pair):
+def write_config(path, max_trades, stake_currency, dry_run_wallet, pair_list):
     read_config(path)
     data['max_open_trades'] = max_trades
     data['stake_currency'] = stake_currency
     data['dry_run_wallet'] = dry_run_wallet
-    data['exchange']['pair_whitelist'] = list(pair)
-
+    data['exchange']['pair_whitelist'] = pair_list
 
     # Güncellenmiş verileri aynı dosyaya geri kaydet
-    with open('crypto\data\config1.json', 'w') as f:
+    with open(path, 'w') as f:
         json.dump(data, f, indent=4)  # indent=4, güzel bir görüntüleme için girinti ekler
 
+if __name__ == "__main__":
+    # Kullanıcıdan girdi al
+    config_path = input("JSON dosyasının yolunu girin: ")
+
+    # Config dosyasını oku ve bilgileri göster
+    read_config(config_path)
+
+    # Yeni değerleri kullanıcıdan al
+    yeni_max_trades = int(input("Yeni max_trades değerini girin: "))
+    yeni_stake_currency = input("Yeni stake_currency değerini girin: ")
+    yeni_dry_run_wallet = float(input("Yeni dry_run_wallet değerini girin: "))
+
+    yeni_pair_listesi = []
+    pair = input("pair girin (örn. ETH/USDT): ")
+    yeni_pair_listesi.append(pair)
+
+    # Config dosyasını güncelle ve kaydet
+    write_config(config_path, yeni_max_trades, yeni_stake_currency, yeni_dry_run_wallet, yeni_pair_listesi)
