91568006bd69d55cc1fc5ed9efc88070fd8c0a89
Kerem Yıldız <yildizkerem8085@gmail.com>
2023-11-10 05:30:27 +0300

    c

--- crypto/strat/strat.py
+++ crypto/strat/strat.py
@@ -1,18 +1,21 @@
 user_data = "/home/fuchsvm/freqtrade/user_data" #user_data path
 strategy_list = ["Diamond", "Heracles", "HourBasedStrategy", "UniversalMACD"]
 
-strategy = "Diamond" #input strateji
-interval = "5m" #input time intervat
+strategy = "UniversalMACD" #input strateji
+interval = "1h" #input time intervat
 
 # Dosyayı açma ve stratejileri yazma
+dosya = []
 with open(f"{user_data}/strategies/{strategy}.py","r+") as strat:
     dosya = strat.readlines()
     row = 0
     for time in dosya:
-        row += 1
         if "timeframe =" in time:
             print(time,row)
             break
-    if row is not None:
-        dosya[row] = interval
-        strat.writelines(dosya)
+        row += 1
+    if row != 0:
+        dosya[row] = "    timeframe = "+f"'{interval}'"
+
+with open(f"crypto/strat/strategies/{strategy}.py","w") as strat:
+    strat.writelines(dosya)
