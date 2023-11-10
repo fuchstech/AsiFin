7a90f3c89c2f72c6d8098f7a61461a7a8185c2bd
Kerem Yıldız <49817710+fuchstech@users.noreply.github.com>
2023-11-10 05:06:40 +0300

    Update strat.py

--- crypto/strat/strat.py
+++ crypto/strat/strat.py
@@ -6,12 +6,13 @@ interval = "5m" #input time intervat
 
 # Dosyayı açma ve stratejileri yazma
 with open(f"{user_data}/strategies/{strategy}.py","r+") as strat:
+    dosya = strat.readlines()
     row = 0
-    for time in strat.readlines():
+    for time in dosya:
         row += 1
         if "timeframe =" in time:
             print(time)
             break
     if row is not None:
-        strat.readlines()[row] = interval
-        strat.writelines(strat)
+        dosya[row] = interval
+        strat.writelines(dosya)
