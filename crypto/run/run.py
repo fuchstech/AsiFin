ed7a41ac2b74f591579a7d1733392eb7b4f0e49c
fuchs_asus <yildizkerem8085@gmail.com>
2023-11-11 21:17:01 +0300

    Update run.py

--- crypto/run/run.py
+++ crypto/run/run.py
@@ -2,5 +2,8 @@
 import os
 
 def run(command):
+    os.system("cd ..")
     print(os.popen('ls').read())
+    os.system("source ./.venv/bin/activate")
+    print(os.popen('freqtrade backtesting -c /home/fuchsvm/Desktop/matris_trade/crypto/conf/data/config.json --strategy Diamond').read())
 run("aa")
