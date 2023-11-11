8e4f8974564276cdb209cc98a807dd7ff5908c20
Kerem Yıldız <49817710+fuchstech@users.noreply.github.com>
2023-11-11 22:26:46 +0300

    Update run.py

--- crypto/run/run.py
+++ crypto/run/run.py
@@ -4,6 +4,5 @@ import os
 def backtest(strategy):
     #print(os.popen('ls').read())
     #os.system("source ./.venv/bin/activate")
-    print(os.popen(f'freqtrade backtesting -c matris_trade.json --strategy {strategy}').read())
+    print(os.popen(f'freqtrade backtesting --strategy {strategy}').read())
 backtest("Diamond")
-
